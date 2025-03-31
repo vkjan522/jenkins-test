518c68ecfddc7acfe1fbed4118dae71ff8680670
vkjan522 <vkjan522@gmail.com>
2025-03-31 15:52:38 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,8 @@
+
+public class Test{
+    public static void main(String[] args) {
+        System.out.println("this is vikash kumar");
+        System.out.println("this code is for jenkins testing");
+        System.out.println(new java.util.Date());
+    }
+}
