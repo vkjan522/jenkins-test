614d639d2c7d366b71accd5f3f959d2f97e7bd45
vkjan522 <vkjan522@gmail.com>
2025-03-31 15:49:58 +0530

    Remove <file> from tracking

--- Test.java
+++
@@ -1,8 +0,0 @@
-
-public class Test{
-    public static void main(String[] args) {
-        System.out.println("this is vikash kumar");
-        System.out.println("this code is for jenkins testing");
-        System.out.println(new java.util.Date());
-    }
-}
